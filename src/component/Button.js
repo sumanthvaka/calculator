938a20b89e5c8f14852f59825932f4228a986b39
Sumanth Vaka (Student) <svaka2@sundevils.asu.edu>
2021-11-05 02:19:52 -0700

    props, state react concept in display pannel example

--- src/component/Button.js
+++ src/component/Button.js
@@ -23,7 +23,7 @@ export default class Button extends React.Component {
 
     return (
       <div className={className.join(" ").trim()}>
-        <button onClick={this.handleClick}>{this.props.name}</button>
+        <button className="clickEffect" onClick={this.handleClick}>{this.props.name}</button>
       </div>
     );
   }
--- src/component/Display.js
+++ src/component/Display.js
@@ -11,7 +11,14 @@ export default class Display extends React.Component {
   render() {
     return (
       <div className="component-display">
-        <div>{this.props.value}</div>
+        <div className="top_div">{this.props.value}</div>
+        <div className="bottom_div">
+          { (this.props.operands.total !=null & this.props.operands.operation !=null)?
+              ((this.props.operands.total!=null ? this.props.operands.total : '')+
+              (this.props.operands.operation!=null ? (this.props.operands.operation === 'x^y' ? '^': this.props.operands.operation ) :'')+
+              (this.props.operands.next!=null ? this.props.operands.next : '')) : ''
+          }
+        </div>
       </div>
     );
   }
