0c58bc08cf828e2d7955240f08186631174587cd
Sumanth Vaka (Student) <svaka2@sundevils.asu.edu>
2021-11-05 02:12:06 -0700

    division replaced power

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -12,13 +12,8 @@ export default function operate(numberOne, numberTwo, operation) {
   if (operation === "x") {
     return one.times(two).toString();
   }
-  if (operation === "÷") {
-    if (two === "0") {
-      alert("Divide by 0 error");
-      return "0";
-    } else {
-      return one.div(two).toString();
-    }
+  if (operation === "x^y") {
+    return Math.pow(one,two)
   }
   throw Error(`Unknown operation '${operation}'`);
 }
